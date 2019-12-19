a7baa88340c742a6d9ac963d487f45acd4bc9bdb
Adam Beardsley <adam.p.beardsley@gmail.com>
2019-12-19 11:46:47 -0700

    basic movement for sprite

--- muck_around/sprite.py
+++ muck_around/sprite.py
@@ -1,9 +1,25 @@
 import numpy as np
 
 
+DIR_DICT = {
+    "ul": np.array([0, 1]),
+    "ur": np.array([1, 1]),
+    "r": np.array([1, 0]),
+    "dr": np.array([0, -1]),
+    "dl": np.array([-1, -1]),
+    "l": np.array([-1, 0]),
+}
+
+
 class Sprite:
     """Class for sprites."""
 
-    def __init__():
+    def __init__(self, location=np.array([0, 0])):
         """Init for sprites."""
-        raise NotImplementedError
+        self.location = location
+
+    def move(self, direction):
+        try:
+            self.location += DIR_DICT[direction]
+        except KeyError:
+            raise ValueError("Direction not understood!")
