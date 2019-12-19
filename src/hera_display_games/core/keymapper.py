8014896079360d7d1bce2f7b418f7ec77767197e
Steven Murray <steven.murray@curtin.edu.au>
2019-12-19 12:25:17 -0700

    It now works

--- src/hera_display_games/core/keymapper.py
+++ src/hera_display_games/core/keymapper.py
@@ -4,14 +4,89 @@
 """A Module to map key inputs."""
 
 import evdev
+from evdev import ecodes
 
-devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-for device in devices:
-    print(device.path, device.name, device.phys)
 
-# TODO: make procedural device mapping
-device = evdev.InputDevice("/dev/input/event2")
+def get_gamepad():
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    for device in devices:
+        print(device.path, device.name, device.phys)
+        if "gamepad" in device.name:
+            mydevice = device
+            print("Using device: ", device.path, device.name, device.phys)
+            break
 
-# starts infinite loop for input
-for event in device.read_loop():
-    print(evdev.categorize(event))
+    try:
+        device = mydevice
+    except NameError:
+        raise IOError("Couldn't find gamepad device")
+
+    return device
+
+
+def get_next_movement(device):
+    for event in device.read_loop():
+        cat = event.type
+        if cat == ecodes.EV_ABS:
+            if ecodes.ABS[event.code] == "ABS_Y":
+                if event.value == 0:
+                    return "u"
+                elif event.value == 255:
+                    return "d"
+                else:
+                    return None  # 'release'
+            else:
+                if event.value == 0:
+                    return "l"
+                elif event.value == 255:
+                    return "r"
+                else:
+                    return None  # 'release'
+
+        elif cat == ecodes.EV_KEY:
+            return
+
+
+#            print(ecodes.BTN[event.code])
+
+
+def map_movement(device):
+    """Return human readable movement condition based on input.
+
+    Returns
+    -------
+    str :
+        Either 'ul', 'ur', 'dl', 'dr', 'l' or 'r'
+    """
+
+    while True:
+        val = get_next_movement(device)
+        # unmapped key press
+        if val is None:
+            continue
+
+        while val in "ud":
+            newval = get_next_movement(device)
+
+            if newval is None:
+                continue
+
+            if newval in "lr":
+                return f"{val}{newval}"
+            elif newval == val:
+                continue
+            elif newval:
+                break
+            else:
+                continue
+
+        if val in "lr":
+            return val
+
+
+if __name__ == "__main__":
+    device = get_gamepad()
+    print(evdev.ecodes.ABS)
+
+    while True:
+        print(map_movement(device))
