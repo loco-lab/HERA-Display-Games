9205e6e055d8470b8fdeca73f76030ca44f6e24d
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-12-19 11:30:18 -0700

    adds a print of devices and captured events

--- src/hera_display_games/core/keymapper.py
+++ src/hera_display_games/core/keymapper.py
@@ -8,3 +8,10 @@ import evdev
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 for device in devices:
     print(device.path, device.name, device.phys)
+
+# TODO: make procedural device mapping
+device = evdev.InputDevice("/dev/input/event2")
+
+# starts infinite loop for input
+for event in device.read_loop():
+    print(evdev.categorize(event))
