108d35aa1d429e9352c415b2626ba631c22c7feb
Steven Murray <steven.murray@curtin.edu.au>
2020-01-03 15:17:13 -0700

    Random initial placement

--- src/hera_display_games/random_walk/__init__.py
+++ src/hera_display_games/random_walk/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """A simple simulation of a random walk"""
 import click
-from hera_display_games.core import mechanics, sprites, keymapper
+from hera_display_games.core import mechanics, sprites, keymapper, map_dict
 import asyncio
 import time
 import random
@@ -40,18 +40,21 @@ def pygame_event_loop(loop, event_queue):
     help="whether to use the physical HERA board",
 )
 def main(speed, nsprites, eat, use_screen):
-    if not 1 <= nsprites <= 6:
-        raise ValueError("nsprites must be between 1 and 6")
+    if not 1 <= nsprites <= 100:
+        raise ValueError("nsprites must be between 1 and 100")
 
     if not 1 <= speed <= 100:
         raise ValueError("speed must be between 1 and 100")
 
+    possible_pos = list(map_dict.reverse_led_map.keys())
+    positions = random.sample(possible_pos, nsprites)
+
     cls = sprites.HungrySprite if eat else sprites.RigidSprite
     my_sprites = []
-    for i in range(nsprites):
+    for i, pos in enumerate(positions):
         my_sprites.append(
             cls(
-                [0, i],
+                list(map_dict.reverse_led_map[pos]),
                 color=(
                     random.randint(100, 255),
                     random.randint(100, 255),
